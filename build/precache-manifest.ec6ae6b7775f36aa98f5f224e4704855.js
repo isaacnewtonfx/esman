10a5652c2f3174ce5ecb3fe1b814879b928089a7
INK <isaacnewtonfx@gmail.com>
2022-03-05 22:36:35 +0000

    removed build dir from gitignore

---
+++ build/precache-manifest.ec6ae6b7775f36aa98f5f224e4704855.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b063fb087a3fa14b8786410c06050d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6526f6dd08f7ecc98939",
+    "url": "/static/css/2.db9d4b9d.chunk.css"
+  },
+  {
+    "revision": "f1e4d4401644c1fa4e2a",
+    "url": "/static/css/main.6ef34d75.chunk.css"
+  },
+  {
+    "revision": "6526f6dd08f7ecc98939",
+    "url": "/static/js/2.ba341e09.chunk.js"
+  },
+  {
+    "revision": "87a8c4cb53762456df69a04c57ce3829",
+    "url": "/static/js/2.ba341e09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e4d4401644c1fa4e2a",
+    "url": "/static/js/main.cc57e4df.chunk.js"
+  },
+  {
+    "revision": "32ce561e861c0f04d287",
+    "url": "/static/js/runtime-main.52155f2b.js"
+  },
+  {
+    "revision": "5f64f1b2f47079318a9729e698c22708",
+    "url": "/static/media/1.5f64f1b2.jpeg"
+  },
+  {
+    "revision": "578f3acd7e4c6393a5363d19398b0237",
+    "url": "/static/media/6.578f3acd.jpeg"
+  },
+  {
+    "revision": "5f64f1b2f47079318a9729e698c22708",
+    "url": "/static/media/7.5f64f1b2.jpeg"
+  },
+  {
+    "revision": "6c38fdb193467c65916c5f6939d48f12",
+    "url": "/static/media/logo.6c38fdb1.webp"
+  }
+]);
